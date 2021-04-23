82f0d0bfe2b81bcbbe08388281193275904bd611
Ken <kensmash@gmail.com>
2021-04-23 10:14:44 -0400

    add cat routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -7,6 +7,15 @@ import Dashboard from "./pages/Dashboard";
 //announcements
 import Announcements from "./pages/Announcements";
 import EditAnnouncement from "./pages/EditAnnouncement";
+//categories
+import Categories from "./pages/Categories/Categories";
+import EditCategory from "./pages/EditCategory/EditCategory";
+import CategoryTypes from "./pages/CategoryTypes/CategoryTypes";
+import EditCategoryType from "./pages/EditCategoryType/EditCategoryType";
+import CategoryGenres from "./pages/CategoryGenres/CategoryGenres";
+import EditCategoryGenre from "./pages/EditCategoryGenre/EditCategoryGenre";
+import CategoryGroups from "./pages/CategoryGroups/CategoryGroups";
+import EditCategoryGroup from "./pages/EditCategoryGroup/EditCategoryGroup";
 
 const Routes = () => (
   <Router>
@@ -17,6 +26,42 @@ const Routes = () => (
         exact
         path="/announcements/:_id"
         component={EditAnnouncement}
+      />
+      <ProtectedRoute exact path={"/categories"} component={Categories} />
+      <ProtectedRoute
+        exact
+        path={"/categories/:_id"}
+        name="Edit Category"
+        component={EditCategory}
+      />
+      <ProtectedRoute exact path={"/categorytypes"} component={CategoryTypes} />
+      <ProtectedRoute
+        exact
+        path={"/categorytypes/:_id"}
+        name="Edit Category Type"
+        component={EditCategoryType}
+      />
+      <ProtectedRoute
+        exact
+        path={"/categorygenres"}
+        component={CategoryGenres}
+      />
+      <ProtectedRoute
+        exact
+        path={"/categorygenres/:_id"}
+        name="Edit Category Genre"
+        component={EditCategoryGenre}
+      />
+      <ProtectedRoute
+        exact
+        path={"/categorygroups"}
+        component={CategoryGroups}
+      />
+      <ProtectedRoute
+        exact
+        path={"/categorygroups/:_id"}
+        name="Edit Category Group"
+        component={EditCategoryGroup}
       />
       <Route path="/login" component={Login} />
     </Switch>
