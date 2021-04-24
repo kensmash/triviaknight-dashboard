0922e69a879da17f8bd81db65a942e35bd3b6892
Ken <kensmash@gmail.com>
2021-04-24 08:41:38 -0400

    set up reactive vars in apollo file

--- client/src/apollo.js
+++ client/src/apollo.js
@@ -29,11 +29,54 @@ const client = new ApolloClient({
     process.env.NODE_ENV === "production" ? "same-origin" : "include",
 });
 
+//reactive vars
+let categorySearchCriteriaVar;
+let categoryGenreSearchCriteriaVar;
 let questionSearchCriteriaVar;
+let addQuestionCriteria;
+
+//check local storage for values
+const savedCategorySearch = JSON.parse(
+  localStorage.getItem("locaCategorySearchCriteria")
+);
+
+const savedCategoryGenreSearch = JSON.parse(
+  localStorage.getItem("locaCategoryGenreSearchCriteria")
+);
 
 const savedQuestionSearch = JSON.parse(
   localStorage.getItem("localQuestionSearchCriteria")
 );
+
+const savedAddQuestionCriteria = JSON.parse(
+  localStorage.getItem("localAddQuestionCriteria")
+);
+
+if (!savedCategorySearch) {
+  categorySearchCriteriaVar = makeVar({
+    activePage: 1,
+    limit: 15,
+    name: "",
+    type: null,
+    genres: [],
+    partycategory: false,
+    showasnew: false,
+    showasupdated: false,
+  });
+} else {
+  categorySearchCriteriaVar = makeVar(savedCategorySearch);
+}
+
+if (!savedCategoryGenreSearch) {
+  categoryGenreSearchCriteriaVar = makeVar({
+    activePage: 1,
+    limit: 15,
+    name: "",
+    types: [],
+  });
+} else {
+  categoryGenreSearchCriteriaVar = makeVar(savedCategoryGenreSearch);
+}
 
 if (!savedQuestionSearch) {
   questionSearchCriteriaVar = makeVar({
@@ -49,4 +92,19 @@ if (!savedQuestionSearch) {
   questionSearchCriteriaVar = makeVar(savedQuestionSearch);
 }
 
-export { client, persistor, questionSearchCriteriaVar };
+if (!savedAddQuestionCriteria) {
+  addQuestionCriteria = makeVar({
+    category: "",
+  });
+} else {
+  addQuestionCriteria = makeVar(savedAddQuestionCriteria);
+}
+
+export {
+  client,
+  persistor,
+  categorySearchCriteriaVar,
+  categoryGenreSearchCriteriaVar,
+  questionSearchCriteriaVar,
+  addQuestionCriteria,
+};
